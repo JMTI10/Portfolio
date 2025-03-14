0c5b9c23611be18cc3afbbb892c9b69761871fbd
JMTI10 <idg@live.com.pt>
2025-03-15 01:14:47 +0200

    .- ..- --. .... .... .... .... ....

--- js/index.js
+++ js/index.js
@@ -14,11 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
     let index = 0;
 
     function changeTitle() {
-        titleElement.style.transform = "translateX(100%)";
+        titleElement.style.transition = "transform 0.5s ease-in-out, opacity 0.5s ease-in-out";
+        titleElement.style.transform = "translateX(20px)";
         titleElement.style.opacity = 0;
         setTimeout(() => {
             titleElement.textContent = titles[index];
-            titleElement.style.transform = "translateX(-100%)";
+            titleElement.style.transform = "translateX(-20px)";
             setTimeout(() => {
                 titleElement.style.transform = "translateX(0)";
                 titleElement.style.opacity = 1;
