7e20235689ba138fb283c9c92f5766153b4c0102
JMTI10 <idg@live.com.pt>
2025-04-07 17:28:55 +0300

    Changes to be committed: 	deleted:    css/header.css 	modified:   css/project.css 	modified:   js/particles.js

--- js/particles.js
+++ js/particles.js
@@ -22,7 +22,7 @@ function createParticles() {
     }
     function animateParticles() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = "#FFD700"; // Golden particles
+        ctx.fillStyle = "#FFD700";
         particles.forEach(p => {
             ctx.beginPath();
             ctx.arc(p.x, p.y, p.radius, 0, Math.PI * 2);
